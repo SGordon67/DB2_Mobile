4b4b3b147c607804fe2cd5de0f089e6cbb241931
Scott Gordon <sgordon031@gmail.com>
2020-04-18 00:46:20 -0400

    sign ni all around works

--- ParentSignIn.php
+++ ParentSignIn.php
@@ -1,5 +1,5 @@
 <?php
-$user = "student";
+$user = "parent";
 $bool = false;
 $mysqli = new mysqli('localhost', 'root', '', 'DB2');
 $email = $_POST['email'];
@@ -10,26 +10,26 @@ $qGetId = "SELECT id FROM users WHERE email = '$email'";
 $id = $mysqli->query($qGetId);
 $targetID = mysqli_fetch_array($id);
 
-// get an array of all student ID's
-$sids = [];
-$qstudentIDs = "SELECT student_id from students";
-$res = $mysqli->query($qstudentIDs);
+// get an array of all parent ID's
+$pids = [];
+$qparentIDs = "SELECT parent_id from parents";
+$res = $mysqli->query($qparentIDs);
 while($row = mysqli_fetch_assoc($res)){
     foreach($row as $cname => $cvalue){
-        array_push($sids,$cvalue);
+        array_push($pids,$cvalue);
     }
 }
 
-// grab student info from user table 
+// grab parent info from user table 
 $qGetInfo = "SELECT * FROM users WHERE email = '$email'";
 $result = $mysqli->query($qGetInfo);
 $testrow = mysqli_fetch_array($result);
 
-// check if target ID is in array of student ID's
+// check if target ID is in array of parent ID's
 if(empty($targetID)){
     $response["success"] = "false";
     echo json_encode($response);
-} else if (!in_array($targetID['id'], $sids)){
+} else if (!in_array($targetID['id'], $pids)){
     $response["success"] = "false";
     echo json_encode($response);
 } else if($password != $testrow['password']){
@@ -39,22 +39,10 @@ if(empty($targetID)){
     // get all of the information from database starting with data in the users table
     // then accessing parent email using parent id
 
-    // get the relevant parent id
-    $getPID = "SELECT parent_id, grade FROM students WHERE student_id = {$targetID['id']}";
-    $pidRes = $mysqli->query($getPID);
-    $pidrow = mysqli_fetch_array($pidRes);
-
-    // get the parent email
-    $getPE = "SELECT email FROM users WHERE id = {$pidrow['parent_id']}";
-    $peRes = $mysqli->query($getPE);
-    $peRow = mysqli_fetch_array($peRes);
-
     $response["name"] = $testrow['name'];
     $response["email"] = $testrow['email'];
     $response["phone"] = $testrow['phone'];
     $response["password"] = $testrow['password'];
-    $response["paEmail"] = $peRow['email'];
-    $response["grade"] = $pidrow['grade'];
 
     $response["success"] = "true";
     echo json_encode($response);
--- RegisterStudent.php
+++ RegisterStudent.php
@@ -1,4 +1,3 @@
-
 <?php
 $name = $_POST['name'];
 // validate email
